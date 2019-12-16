4e2ee464b77386cabd15d3e2f6a6e651ee226b80
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2019-12-16 23:00:56 +0530

    added a new module

--- app/src/main/java/com/example/conduct/MainActivity.java
+++ app/src/main/java/com/example/conduct/MainActivity.java
@@ -20,7 +20,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.example.conduct.sync.wifiDirectBroadcastReceiver;
 import com.example.conduct.utils.LocaleManager;
 
 import java.util.ArrayList;
@@ -29,47 +28,6 @@ import java.util.List;
 public class MainActivity extends BaseActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     SharedPreferences sharedPreferences;
-    WifiManager wifiManager;
-    WifiP2pManager mManager;
-    WifiP2pManager.Channel mChannel;
-    BroadcastReceiver mReceiver;
-    IntentFilter mIntentFilter;
-
-    List<WifiP2pDevice> peers = new ArrayList<WifiP2pDevice>();
-    String[] deviceNameArray;
-    WifiP2pDevice[] deviceArray;
-
-    ListView devices;
-
-    // check the wifi state and tell if the wifi is on off . Also turns the wifi on/off
-    private void wifiState() {
-
-        if (wifiManager.isWifiEnabled()) {
-            wifiManager.setWifiEnabled(false);
-        } else {
-            wifiManager.setWifiEnabled(true);
-        }
-    }
-
-    // discover peers
-    private void discoverPeers() {
-
-        if (sharedPreferences.getBoolean(getString(R.string.discover_peers_key), false) == true) {
-            mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
-                @Override
-                public void onSuccess() {
-                    Toast.makeText(MainActivity.this, "Finding Peeers", Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onFailure(int reason) {
-                    Toast.makeText(MainActivity.this, "Discovery Failed", Toast.LENGTH_SHORT).show();
-                }
-            });
-        } else {
-            Toast.makeText(this, "Finding Peers Stopped", Toast.LENGTH_SHORT).show();
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,66 +36,6 @@ public class MainActivity extends BaseActivity implements SharedPreferences.OnSh
 
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         sharedPreferences.registerOnSharedPreferenceChangeListener(this);
-
-        wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-        mManager = (WifiP2pManager) getSystemService(WIFI_P2P_SERVICE);
-        mChannel = mManager.initialize(this, getMainLooper(), null);
-
-
-        mReceiver = new wifiDirectBroadcastReceiver(mManager, mChannel, this);
-        mIntentFilter = new IntentFilter();
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-        devices = (ListView) findViewById(R.id.device_list_array);
-//        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1);
-//        adapter1.add("Hello");
-//        devices.setAdapter(adapter1);
-    }
-
-    // now listen to different devices - peers
-    public WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
-        @Override
-        public void onPeersAvailable(WifiP2pDeviceList peerList) {
-            // if the array is less than the new peerDevice list ie. if a new device is added or removed,
-            if (!peerList.getDeviceList().equals(peers)) {
-
-                // clear the original array
-                peers.clear();
-                // add new items.
-                peers.addAll(peerList.getDeviceList());
-
-                // initialize the new arrays.
-                deviceNameArray = new String[peerList.getDeviceList().size()];
-                deviceArray = new WifiP2pDevice[peerList.getDeviceList().size()];
-                int index = 0;
-
-                for (WifiP2pDevice device: peerList.getDeviceList()) {
-                    deviceNameArray[index] = device.deviceName;
-                    deviceArray[index] = device;
-                    index++;
-                }
-
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, deviceNameArray);
-                if (adapter == null) {
-                    Toast.makeText(getApplicationContext(), "No Devices Found", Toast.LENGTH_SHORT).show();
-                    ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, deviceNameArray);
-                    adapter1.add("Hello");
-                    devices.setAdapter(adapter1);
-                    return;
-
-                } else {
-                    devices.setAdapter(adapter);
-                }
-
-                if (peers.size() == 0) {
-                    Toast.makeText(getApplicationContext(), "No Devices Found", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-            }
-        }
     };
 
     @Override
@@ -173,14 +71,6 @@ public class MainActivity extends BaseActivity implements SharedPreferences.OnSh
         if (key.equals(getString(R.string.language_preference))) {
             Context context = LocaleManager.setLocale(MainActivity.this, sharedPreferences.getString(getString(R.string.language_preference), getString(R.string.language_english_value)));
             recreate();
-        }
-
-        if (key.equals(getString(R.string.wifi_key))) {
-            wifiState();
-        }
-
-        if (key.equals(getString(R.string.discover_peers_key))) {
-            discoverPeers();
         }
     }
 }
